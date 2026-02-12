4aa748d782b2af3ebd3e3e3dbc61d7e4b63148f7
Carlos Chacon <cchacon@promarketingchile.com>
2026-02-12 06:38:05 -0500

    Add Prisma seeding functionality and update database schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   schema: 'prisma',
   migrations: {
     path: 'prisma/migrations',
+    seed: 'ts-node --project prisma/tsconfig.json prisma/seed.ts',
   },
   datasource: {
     url: process.env.DATABASE_URL,
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Global() // Esto lo hace disponible en toda la app
+@Global()
 @Module({
-    providers: [PrismaService],
-    exports: [PrismaService],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
-export class PrismaModule { }
+export class PrismaModule {}
